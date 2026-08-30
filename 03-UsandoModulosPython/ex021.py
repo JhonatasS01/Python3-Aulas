ee636a8650db0a5f9e7e355d57cefcfc6bbe4e0c
Jhonatas <diversos.chevy455@simplelogin.com>
2026-08-30 12:08:48 -0500

    Bug fixes and improvements

--- 03-UsandoModulosPython/ex021.py
+++ 03-UsandoModulosPython/ex021.py
@@ -1,7 +1,7 @@
 import pygame
 
 pygame.init()
-pygame.mixer_music.load("data/Saiba-DUAS-COISAS.mp3")
+pygame.mixer_music.load("date/Saiba-DUAS-COISAS.mp3")
 pygame.mixer_music.play()
 pygame.event.wait()
 input("Parar a música?")
