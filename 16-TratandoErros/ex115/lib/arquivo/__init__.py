8bb7dd76e034c9be6bb133b780d83812e6267e8c
Jhonatas <diversos.chevy455@simplelogin.com>
2026-08-30 13:13:44 -0500

    Improvements

--- 16-TratandoErros/ex115/lib/arquivo/__init__.py
+++ 16-TratandoErros/ex115/lib/arquivo/__init__.py
@@ -1,6 +1,11 @@
 from ex115.lib.interface import *
 
 def arq_existe(nome):
+    """
+    -> Função para verificar se o arquivo do ex115 existe ou não.
+    :param nome: Nome do arquivo.
+    :return: Retorna False ou True.
+    """
     try:
         a = open(nome, 'rt')
         a.close()
@@ -10,6 +15,11 @@ def arq_existe(nome):
         return True
 
 def arq_create(nome):
+    """
+    -> Função para criar o arquivo do ex115.
+    :param nome: Nome do arquivo a ser criado.
+    :return: Retorna o arquivo.txt na raiz do projeto.
+    """
     try:
         a = open(nome, 'wt+')
         a.close()
@@ -19,6 +29,11 @@ def arq_create(nome):
         print(f'Arquivo {nome} criado com sucesso!')
 
 def arq_writer(nome):
+    """
+    -> Função para ler o arquivo do ex115.
+    :param nome: Nome do arquivo a ser lido.
+    :return: Retorna os dados do arquivo com o cabeçalho formatado.
+    """
     try:
         a = open(nome, 'rt')
     except FileNotFoundError:
@@ -31,6 +46,13 @@ def arq_writer(nome):
             print(f'{dado[0]:<30}{dado[1]:>3} anos')
 
 def cadastrar(arq, nome='desconhecido', idade=0):
+    """
+    -> Função para cadastrar as pessoas no arquivo do ex115.
+    :param arq: Arquivo para ser feito o cadastro.
+    :param nome: Nome da pessoa.
+    :param idade: Idade da pessoa.
+    :return: Retorna uma mensagem da situação do cadastro.
+    """
     try:
         a = open(arq, 'at')
     except FileNotFoundError:
